9b52f169f79c31b35c358248f13e4de6ef2438c9
saha rick <jayitsaha@gmail.com>
2020-07-25 17:32:46 +0530

    Picking Numbers

--- PickingNumbers.java
+++ PickingNumbers.java
@@ -106,43 +106,47 @@
 //    
 //    
 //}
-public static int pickingNumbers(List<Integer> a) {
-        int frequency[] = new int[101];
-        int result = Integer.MIN_VALUE;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
+import java.util.Set;
 
-        for (int i = 0; i < a.size(); i++) {
-            int index=a.get(i);
-            frequency[index]++; //frequency[index]=frequency[index]+1
-        }
 
-        for (int i = 1; i <= 100; i++) {
-            result = Math.max(result, frequency[i] + frequency[i - 1]);
-        }
-        return result;
+public class PickingNumbers {
 
-    }
-        
-        
-        
-        
-        
-    
+	public static int pickingNumbers(List<Integer> a) {
+		int frequency[] = new int[101];
+		int result = Integer.MIN_VALUE;
 
-}
+		for (int i = 0; i < a.size(); i++) {
+			int index=a.get(i);
+			frequency[index]++; //frequency[index]=frequency[index]+1
+		}
 
-public class Solution {
-    public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			result = Math.max(result, frequency[i] + frequency[i - 1]);
+		}
+		return result;
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
-        while (n-- > 0) {
-            list.add(sc.nextInt());
-        }
+	}
 
-        int result = Result.pickingNumbers(list);
-        System.out.println(result);
-        sc.close();
+	public static void main(String[] args) {
 
-    }
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+		while (n-- > 0) {
+			list.add(sc.nextInt());
+		}
+
+		int result = pickingNumbers(list);
+		System.out.println(result);
+		sc.close();
+
+	}
 }
